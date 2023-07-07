959810c480a236ce2ba9c055b7ed67b7b67ca5b7
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2023-07-07 14:19:03 +0300

    Rewrite createGrid function
    Removed global variables, put them in the function.
    
    Removed the unnecessary true argument from gridElement.cloneNode().True
    argument ensures that all child elements within gridElement are also cloned.
    
    Rewrote variable names for convenience

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-const gridContainer = document.getElementById("grid-container");
-
-const gridRow = document.createElement("div");
-gridRow.classList.add("grid-row");
-
-const gridElement = document.createElement("div");
-gridElement.classList.add("grid-element");
-
-createGrid(8); //user's input
-
 function createGrid(size) {
-    // add elemetns to gridRow
-    for(let i = 0; i < size; i++) {
-        const copy = gridElement.cloneNode(true);
-        gridRow.appendChild(copy);
+    const gridContainer = document.getElementById("grid-container");
+
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("grid-row");
+
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+
+    // Add elements to gridRow
+    for (let i = 0; i < size; i++) {
+        const element = gridElement.cloneNode();
+        gridRow.appendChild(element);
     }
-    // add elemetns to gridContainer
-    for(let i = 0; i < size; i++) {
-        const copy = gridRow.cloneNode(true);
-        gridContainer.appendChild(copy);
+
+    // Add rows to gridContainer
+    for (let i = 0; i < size; i++) {
+        const row = gridRow.cloneNode(true);
+        gridContainer.appendChild(row);
     }
 }
+
+createGrid(8);
